05a119cb4116d69b30f980c7c18d251543f7d752
shalomshushan <aweandthansome@gmail.com>
2020-03-20 22:11:55 -0500

    first commit

---
+++ src/test/java/com/config/server/learning/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.config.server.learning.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
